03274e14c4c6e10f51d013650c33e4cc188a5199
Architmaurya <architmaurya7054@gmail.com>
2025-12-06 15:55:11 +0530

    <fix>

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(
   cors({
